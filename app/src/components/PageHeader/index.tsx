8c2eb8ed5e6affedbbeffc068ba55b193c195178
Rickson <cyberickson@gmail.com>
2020-08-07 22:23:47 -0300

    Next Level Week 02 finalizada

--- app/src/components/PageHeader/index.tsx
+++ app/src/components/PageHeader/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
-import backIcon from '../../../assets/images/icons/back.png';
-import logoImg from '../../../assets/images/logo.png';
+import backIcon from '../../assets/images/icons/back.png';
+import logoImg from '../../assets/images/logo.png';
 
 import styles from './styles';
 import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps{
     title: string;
+    headerRight?: ReactNode;
 
 }
 
-const PageHeader: React.FunctionComponent<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FunctionComponent<PageHeaderProps> = ({ title, children, headerRight }) => {
     const { navigate } = useNavigation();
     
     function handleGoBack(){
@@ -30,7 +31,15 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = ({ title }) => {
 
             <Image source={logoImg} resizeMode="contain"/>
         </View>
+        
+        <View style={styles.header}>
+
         <Text style={styles.title}>{title}</Text>
+
+        {headerRight}
+        </View>
+
+        {children}
     </View>
     )
 }
